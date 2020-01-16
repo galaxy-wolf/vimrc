ffae0cb79df8529c94ca79f52b232a52b2b8f823
ZhaoQi <964498718@qq.com>
2020-01-16 22:22:42 +0800

    1. support python3 2. when use command 'ping', 192.168.0.1 may responses for invalid IP, ignore it.

--- c18.py
+++ c18.py
@@ -2,12 +2,28 @@ import subprocess
 import os
 
 USER_NAME = 'zhaoqi'
+BAD_IP = '192.168.0.1'
+
+def get_percent(s):
+    ret = ''
+    for c in s:
+        if '0' <= c <= '9':
+            ret += c
+    return int(ret)
+
+
+def get_ip_from_str(s):
+    ret = ''
+    for c in s:
+        if '0' <= c <= '9' or c == '.':
+            ret += c
+    return ret
 
 
 def get_my_ip():
     # b'192.168.1.1\\n'
     ret = subprocess.Popen("ipconfig | grep -ia 'IPv4' | sed -n 2p | grep -iao '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'", shell=True, stdout=subprocess.PIPE).stdout.read()
-    ret = str(ret)[:-1]
+    ret = get_ip_from_str(str(ret))
     return ret, ret[:ret.rfind('.')], ret[ret.rfind('.')+1:]
 
 
@@ -15,19 +31,25 @@ def get_u18_ip():
     my_ip, a, b = get_my_ip()
     print('my ip is %r %r %r' % (my_ip, a, b))
     b = int(b)
-    for bb in xrange(b+1, 256):
+    for bb in range(b+1, 256):
         ip = '{a}.{b}'.format(a=a, b=str(bb))
         if is_good_ip(ip):
             return ip
         print('test %r failed' % ip)
     return ''
-        
+
 
 def is_good_ip(ip):
+    cmd = "ping " + str(ip) + " -i 1 -w 1000 -n 1"
+    ret = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read()
+    if BAD_IP in str(ret):
+        return False
+
+    cmd = "ping " + str(ip) + " -i 1 -w 1000 -n 1 | grep -iao '[0-9]\{1,3\}%'"
     # -i 1 TTL
-    ret = subprocess.Popen("ping " + str(ip) + " -i 1 -w 1000 -n 1 | grep -iao '[0-9]\{1,3\}%'", shell=True, stdout=subprocess.PIPE).stdout.read()
+    ret = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read()
     ret = str(ret)
-    ret = int(ret[:-2])
+    ret = get_percent(ret)
     if ret > 0:
         return False
     return True
